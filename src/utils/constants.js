5c0d94c11e3ede15b390fda4ce74f0c93f69c4aa
nguyen.hailong <nguyenhailong2408@gmail.com>
2025-02-07 21:23:32 +0700

    update domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,9 @@
 //Những domain được phép truy cập server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173',
-  'https://trello-reactjs.vercel.app/',
-  'https://trello-reactjs-git-master-longnhs-projects-9f4f359b.vercel.app/'
+  'https://trello-reactjs.vercel.app',
+  'https://trello-reactjs-git-master-longnhs-projects-9f4f359b.vercel.app',
+  'https://trello-reactjs-3cgjj6wu1-longnhs-projects-9f4f359b.vercel.app'
 ]
 
 export const BOARD_TYPES = {
